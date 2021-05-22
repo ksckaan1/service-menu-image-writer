69f8a8140c297f0ba2a7e31fd58f937b6ac3b3c9
Kaan Kuscu <33625775+ksckaan1@users.noreply.github.com>
2021-05-23 01:34:13 +0300

    initialize

---
+++ install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo "Installing Image Writer for Dolphin Extension"
+cp ./imageWriter.desktop ${HOME}/.local/share/kservices5/ServiceMenus/
+echo "Successfuly installed!"
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo "Uninstalling Image Writer for Dolphin Extension"
+rm ${HOME}/.local/share/kservices5/ServiceMenus/imageWriter.desktop
+echo "Successfuly uninstalled!"
